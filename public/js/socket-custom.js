2582f8e7812b02a34b569b23db8b2d6e9d815e95
Xavi3R90 <javisanchezv90@gmail.com>
2019-05-29 11:22:57 -0400

    Primer Commit

---
+++ public/js/socket-custom.js
@@ -0,0 +1,27 @@
+var socket = io();
+
+socket.on('connect', function() {
+
+    console.log('Conectado al servidor');
+
+});
+
+// Escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos la conexión con el servidor');
+
+});
+
+// Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Fernando',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});
+
+// Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+    console.log('Servidor: ', mensaje);
+});
